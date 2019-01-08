6f10a62695142b43eb2e56bf4b4229e0339cd9fa
aeferguson <46054196+aeferguson@users.noreply.github.com>
2019-01-08 13:20:25 -0600

    Initial commit

---
+++ start-stop_instances.py
@@ -0,0 +1,42 @@
+import sys
+import boto3
+from botocore.exceptions import ClientError
+
+instance_id = sys.argv[2]
+action = sys.argv[1].upper()
+
+# # Each time an instance is started, Amazon EC2 charges a full instance hour, even if multiple starts happen within a single hour.
+# # will need to specify power state and instance-id when running, ex:
+# # > python ./start-stop_instances.py [ON|OFF] i-934857394857 
+
+ec2 = boto3.client('ec2')
+
+
+if action == 'ON':
+    # Do a dryrun first to verify permissions
+    try:
+        ec2.start_instances(InstanceIds=[instance_id], DryRun=True)
+    except ClientError as e:
+        if 'DryRunOperation' not in str(e):
+            raise
+
+    # Dry run succeeded, run start_instances without dryrun
+    try:
+        response = ec2.start_instances(InstanceIds=[instance_id], DryRun=False)
+        print(response)
+    except ClientError as e:
+        print(e)
+else:
+    # Do a dryrun first to verify permissions
+    try:
+        ec2.stop_instances(InstanceIds=[instance_id], DryRun=True)
+    except ClientError as e:
+        if 'DryRunOperation' not in str(e):
+            raise
+
+    # Dry run succeeded, call stop_instances without dryrun
+    try:
+        response = ec2.stop_instances(InstanceIds=[instance_id], DryRun=False)
+        print(response)
+    except ClientError as e:
+        print(e)
